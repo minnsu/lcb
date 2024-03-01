3ab210228a2f551d9bfe316c0ac1e74f0f4bc61c
minsu <kms48491000@gmail.com>
2024-03-01 11:39:27 +0000

    2024/03/01 .

--- dev/langchain/lc_package/chain.py
+++ dev/langchain/lc_package/chain.py
@@ -3,10 +3,14 @@ from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from langchain.chains import ConversationChain, ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
-from lc_package.memory import save_memory, load_memory
+from lc_package.utils import save_memory, load_memory
+
+default_role = """Use the following pieces of context and chat history to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Don't be wordy, just answer what is asked."""
 
 default_template = """
-Your Role: Use the following pieces of context and chat history to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Your Role: {role}
 
 [Context]
 {context}
@@ -19,15 +23,15 @@ Question: {question}
 Helpful Answer:
 """
 
-def make_default_prompt():
+def default_prompt():
     return PromptTemplate.from_template(template=default_template)
 
-def make_chain(llm, prompt=None):
+def make_llm_chain(llm, prompt=None):
     if prompt is None:
-        prompt = make_default_prompt()
+        prompt = default_prompt()
     return (prompt | llm)
 
-def make_context(db, text, limit: int=3):
+def make_context(db, text, limit: int=10):
     searched = db.similarity_search(text)
     str_searched = ''
     for i in range(min(len(searched), limit)):
@@ -35,10 +39,12 @@ def make_context(db, text, limit: int=3):
     return str_searched
 
 def make_history(memory):
-    return memory.buffer_as_str
+    return memory.buffer_as_str # memory: ConversationBufferMemory
 
-def invoke(chain, db, memory, question):
-    ret = chain.invoke({'context': make_context(db, question), 'chat_history': make_history(memory), 'question': question})
+def invoke(llm_chain, db, memory, question, role: str=None):
+    if role is None:
+        role = default_role
+    ret = llm_chain.invoke({'role': role, 'context': make_context(db, question), 'chat_history': make_history(memory), 'question': question})
 
     memory.buffer_as_messages.append(HumanMessage(question))
     memory.buffer_as_messages.append(AIMessage(ret))
--- dev/langchain/lc_package/pipe.py
+++ dev/langchain/lc_package/pipe.py
@@ -7,25 +7,11 @@ def get_model_and_tokenizer(model_path, load_in_8bit: bool=False):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     return (model, tokenizer)
     
-
-def make_llm_pipe(model_path: str, task: str, max_new_tokens: int=512, temperature: float=0.6, top_p: float=0.95, repetition_penalty: float=1.2, load_in_8bit: bool=False):
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto', torch_dtype=torch.bfloat16, load_in_8bit=load_in_8bit)
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
+def adjust_option(model, tokenizer, task: str, max_new_tokens: int=512, temperature: float=0.6, top_p: float=0.95, repetition_penalty: float=1.2):
     hf_pipe = pipeline(task=task, model=model, tokenizer=tokenizer, max_new_tokens=max_new_tokens, do_sample=True, temperature=temperature, top_p=top_p, repetition_penalty=repetition_penalty)
     lc_pipe = HuggingFacePipeline(pipeline=hf_pipe)
     return lc_pipe
 
-def make_llm_pipe_fast(model_path: str, task: str, max_new_tokens: int=512, temperature: float=0.6, top_p: float=0.95, repetition_penalty: float=1.2):
-    lc_pipe = HuggingFacePipeline.from_model_id(
-        model_id=model_path,
-        device=0,
-        task=task,
-        pipeline_kwargs={
-            'max_new_tokens': max_new_tokens,
-            'do_sample': True,
-            'temperature': temperature,
-            'top_p': top_p,
-            'repetition_penalty': repetition_penalty
-        }
-    )
-    return lc_pipe
+def make_llm(model_path: str, task: str, max_new_tokens: int=512, temperature: float=0.6, top_p: float=0.95, repetition_penalty: float=1.2, load_in_8bit: bool=False):
+    model, tokenizer = get_model_and_tokenizer(model_path, load_in_8bit)
+    return adjust_option(model, tokenizer, task, max_new_tokens, temperature, top_p, repetition_penalty)
